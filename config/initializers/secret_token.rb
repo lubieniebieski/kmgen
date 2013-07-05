11b8533faa645491570485d676dc53c5e73e8e68
Adam Nowak <adam.nowak@netguru.pl>
2013-07-05 21:50:59 +0200

    project init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kmgen::Application.config.secret_key_base = 'd9465d906bd415a9fe1447658391739cf52598056cb5423fcdbed9f8f084b4fb37a0b3636185a9f17f38c27f3e838d64fdacd175038c9c11c18ef4a3fd66413f'
